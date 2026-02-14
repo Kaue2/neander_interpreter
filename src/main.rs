97b65333c6d7aadd22b958d25f69b14a93c9a9f3
Kauê Oliveira dos Anjos <kaueo.anjos@gmail.com>
2026-02-13 22:42:38 -0300

    add: adicionar função para processar (ainda está quebrado)

--- src/main.rs
+++ src/main.rs
@@ -5,39 +5,62 @@ use std::io::{Read};
 use std::process;
 
 struct  ErrorInvalidFormat;
-type InstructionFn = fn(&mut Interpreter, u8);
+type InstructionFn = fn(&mut Interpreter, usize);
 
+#[derive(Debug)]
 struct Interpreter {
-    pub ac: u8,
-    pub pc: usize,
-    pub memory: Vec<u8>,
-    pub zero: bool,
-    pub negative: bool,
+    ac: u8,
+    pc: usize,
+    memory: Vec<u8>,
+    zero: bool,
+    negative: bool,
+    exit: bool, 
+}
+
+impl Default for Interpreter {
+    fn default() -> Self {
+        Self {
+            ac: 0,
+            pc: 4,
+            memory: Vec::new(),
+            zero: true,
+            negative: false,
+            exit: false,
+        }
+    }
 }
 
 // funções
 
-fn nop(_i: &mut Interpreter, _val: u8) {
+fn nop(_i: &mut Interpreter, _address: usize) {
+    println!("Função nop");
     return;
 }
 
-fn add(i: &mut Interpreter, val: u8) {
-    i.ac.saturating_add(val);
+fn add(i: &mut Interpreter, address: usize) {
+    println!("Função add");
+    let val = i.memory[address];
+    i.ac = i.ac.saturating_add(val);
 }
 
-fn get_rules() -> HashMap<u8, InstructionFn> {
+fn halt(i: &mut Interpreter, _address: usize) {
+    println!("Função halt");
+    i.exit = true;
+}
+
+fn get_rules() -> HashMap<u8, InstructionFn> { 
     let mut m: HashMap<u8, InstructionFn> = HashMap::new();
     m.insert(0, nop);
-    m.insert(16, todo!());
-    m.insert(32, todo!());
+    m.insert(16, nop);
+    m.insert(32, nop);
     m.insert(48, add);
-    m.insert(64, todo!());
-    m.insert(80, todo!());
-    m.insert(96, todo!());
-    m.insert(128, todo!());
-    m.insert(144, todo!());
-    m.insert(160, todo!());
-    m.insert(240, todo!());
+    m.insert(64, nop);
+    m.insert(80, nop);
+    m.insert(96, nop);
+    m.insert(128, nop);
+    m.insert(144, nop);
+    m.insert(160, nop);
+    m.insert(240, halt);
 
 
     return m;
@@ -52,27 +75,45 @@ impl Interpreter {
         if !buffer.starts_with(&required_format) {
             return Err(ErrorInvalidFormat);
         }
-
+        
         let i = Interpreter { 
             ac:         0,
-            pc:         0,
+            pc:         4,
             memory:     buffer,
             zero:       true,
-            negative:   false 
+            negative:   false,
+            exit: false,
         };
 
         return Ok(i);
     }
 
+    // vai pra próxima linha de instrução
+    fn next(&mut self) {
+        self.pc += 2;
+    }
+
     pub fn process(&mut self, rules: HashMap<u8, InstructionFn>) {
-        let mut instruction = true;
-        for pair in self.memory.chunks(2) .skip(2) { // pulando 2 primeiros pares
-            match rules.get(&pair[0]) {
-                Some(_) => {},
-                None => {}
-            }
-            println!("{} {}", pair[0], pair[1])
+        while !self.exit && self.pc < self.memory.len() {
+            let opcode_or_address = self.memory[self.pc];
+            println!("code: {}", opcode_or_address);
+            if let Some(instruction) = rules.get(&opcode_or_address) {
+                if opcode_or_address == 0 || opcode_or_address == 240 {
+                    instruction(self, 0);
+                    self.next();
+                } else {
+                    self.next();
+                    println!("pc: {}", self.pc);
+                    let address = usize::from(self.memory[self.pc] * 2 + 4); // pega a linha e converte pro endereço real do array
+                    instruction(self, address);
+                }
+            };
+            
         }
+    }
+
+    pub fn calculate_pc(&self) -> u8 {
+        return ((self.pc.saturating_sub(4)) / 2) as u8;
     }
 }
 
@@ -80,7 +121,7 @@ impl fmt::Display for Interpreter {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         writeln!(f, "Interpreter state:")?;
         writeln!(f, "AC: {}", self.ac)?;
-        writeln!(f, "PC: {}", self.pc)?;
+        writeln!(f, "PC: {}", self.calculate_pc())?;
         writeln!(f, "Memory:")?;
 
         for (i, byte) in self.memory.iter().enumerate() {
@@ -97,6 +138,7 @@ impl fmt::Display for Interpreter {
 
 
 fn main() {
+    /*
     let file = File::open("./exemplo.bin").unwrap();
     let rules = get_rules();
     let mut inter: Interpreter = match Interpreter::new(file) {
@@ -106,7 +148,30 @@ fn main() {
             process::exit(1);
         }
     };
+     */
+    let rules = get_rules();
+    let mut inter: Interpreter = Default::default();
+    inter.memory = Vec::from([3, 78, 68, 82, 0, 0, 48, 0, 3, 0, 33, 0, 240, 0]);
+    inter.pc = 4;
     
     println!("{}", inter);
     inter.process(rules);
+}
+
+#[cfg(test)]
+mod tests {
+    use std::default;
+
+    use crate::{Interpreter, get_rules};
+
+
+    #[test]
+    fn rules() {
+        let mut i = Interpreter::default();
+        i.memory = Vec::from([3, 78, 68, 82, 0, 0, 48, 0, 3, 0, 33, 0, 240, 0]);
+        i.pc = 4;
+        i.process(get_rules());
+
+        assert_eq!(i.ac, 33);
+    }
 }
